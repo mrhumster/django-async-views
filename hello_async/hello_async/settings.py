76445a0010c1428665f6bceba99fac957f0e95a6
XOMRKOB <mr.humster@gmail.com>
2022-04-30 23:24:59 +0600

    remove secret key

--- hello_async/hello_async/settings.py
+++ hello_async/hello_async/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from hello_async.secret import KEY
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rs-yt-jwen-vl!899s-6&96f@_!9g*60k_x5toz_0rc%*!b=bd'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
